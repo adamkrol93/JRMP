fd71cb76e0ca9b650b7140f288ad48c1b9a1290a
jonatan <180547@edu.p.lodz.pl>
2015-04-18 23:52:13 +0200

    PIP-53

---
+++ src/main/java/net/amg/jira/plugins/rest/RESTRiskManagementResourceModel.java
@@ -0,0 +1,25 @@
+package net.amg.jira.plugins.rest;
+
+import javax.xml.bind.annotation.*;
+@XmlRootElement(name = "message")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class RESTRiskManagementResourceModel {
+
+    @XmlElement(name = "value")
+    private String message;
+
+    public RESTRiskManagementResourceModel() {
+    }
+
+    public RESTRiskManagementResourceModel(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
