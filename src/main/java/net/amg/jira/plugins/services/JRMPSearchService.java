d7d3fbac8b6ea132756b4de7aad9786959708098
Adam Król <adam.krol93@gmail.com>
2015-05-10 22:40:33 +0200

    PIP-116: Usułga wyszukująca wszystkie zgłoszenia, które są znaczące z punktu widzenia wtyczki.

---
+++ src/main/java/net/amg/jira/plugins/services/JRMPSearchService.java
@@ -0,0 +1,16 @@
+package net.amg.jira.plugins.services;
+
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.query.Query;
+import net.amg.jira.plugins.exceptions.NoIssuesFoundException;
+
+import java.util.List;
+
+/**
+ * Usługa znajdująca wszystkie potrzebne taski w jire. Najczęściej na podstawie Query.
+ * @author Adam Król
+ */
+public interface JRMPSearchService {
+
+    List<Issue> getAllQualifiedIssues(Query query) throws NoIssuesFoundException;
+}
