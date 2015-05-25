74a49aede53466891ead442c4d3d13e225b57c48
Adam Król <adam.krol93@gmail.com>
2015-05-25 16:45:52 +0200

    PIP-144 : Dodana licenacja dla pluginu

--- src/main/java/net/amg/jira/plugins/jrmp/services/JRMPSearchServiceImpl.java
+++ src/main/java/net/amg/jira/plugins/jrmp/services/JRMPSearchServiceImpl.java
@@ -1,3 +1,17 @@
+/*Copyright 2015 AMG.net
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 package net.amg.jira.plugins.jrmp.services;
 
 import com.atlassian.jira.bc.issue.search.SearchService;
@@ -31,12 +45,6 @@ public class JRMPSearchServiceImpl implements JRMPSearchService {
     private CustomFieldManager customFieldManager;
 
     private Logger logger = LoggerFactory.getLogger(getClass());
-
-//    public JRMPSearchServiceImpl(SearchService searchService, JiraAuthenticationContext authenticationContext, CustomFieldManager customFieldManager) {
-//        this.searchService = searchService;
-//        this.authenticationContext = authenticationContext;
-//        this.customFieldManager = customFieldManager;
-//    }
 
     @Override
     public List<Issue> getAllQualifiedIssues(Query query) {
